1649109a2caa8b8ba6c2132fd76bc52fc7673219
kmav <kmavroudakis@gmail.com>
2014-03-27 20:27:00 +0200

    Initial Commit

---
+++ InstaPhoto/IPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPAppDelegate.h
+//  InstaPhoto
+//
+//  Created by Konstantinos M on 27/3/14.
+//  Copyright (c) 2014 com.lumicsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstaPhoto
+//
+//  Created by Konstantinos M on 27/3/14.
+//  Copyright (c) 2014 com.lumicsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPAppDelegate class]));
+    }
+}
